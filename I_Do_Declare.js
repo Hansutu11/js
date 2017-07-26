a150a95b0364e6103cab97dceded36914a51a27c
Hans Utu <utuhans@gmail.com>
2017-07-25 21:15:50 -0700

    new file

---
+++ I_Do_Declare.js
@@ -0,0 +1,12 @@
+var num = [24,36,48,100]
+var fristName="Hans";
+var string2 ="Minnons";
+var string3= "Music is cool";
+var codingisfun = true;
+var codingisLame = false;
+
+console.log(num);
+console.log(fristName);
+console.log(string2);
+console.log(string3);
+console.log(codingisfun);
