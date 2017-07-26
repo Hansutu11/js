02e646d57331876bc7b595cdca79ebcb96b9a637
Hans Utu <utuhans@gmail.com>
2017-07-25 21:52:54 -0700

    added Time.js file

---
+++ TIme.js
@@ -0,0 +1,11 @@
+var hour = 7;
+var minute = 15;
+var period = "PM";
+
+console.log(hour, minute, period);
+if (period == "AM") {
+  console.log("It's almost 9 in the morning");
+}
+else {
+  console.log("It's just after 7 in the evening");;
+}
